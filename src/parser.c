567c519c0433f56e30bd2db8f5dfe66e164c403e
BubbleFish <daforsastudia@gmail.com>
2026-03-03 22:36:32 +0300

    chore: Update version

--- src/parser.c
+++ src/parser.c
@@ -20533,8 +20533,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_mcfunction(void) {
     .name = "mcfunction",
     .max_reserved_word_set_size = 0,
     .metadata = {
-      .major_version = 1,
-      .minor_version = 0,
+      .major_version = 0,
+      .minor_version = 2,
       .patch_version = 0,
     },
   };
