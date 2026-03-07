d8fa6f13f7fb185898cd8759805dc6b9564f9955
BubbleFish <daforsastudia@gmail.com>
2026-03-08 00:09:26 +0300

    feat(grammar): Add hexadecimal

--- grammar/02_primitive.js
+++ grammar/02_primitive.js
@@ -1,7 +1,8 @@
 const PREC_BUILTIN = 4;
 
 module.exports = {
-	_primitive_type: ($) => choice($.boolean, $.float, $.integer),
+	_primitive_type: ($) =>
+		choice($.boolean, $.float, $.integer, $.hexadecimal),
 
 	boolean: (_) => token(prec(PREC_BUILTIN, choice("true", "false"))),
 
@@ -17,6 +18,12 @@ module.exports = {
 			token(prec(PREC_BUILTIN, /\.\d+/)),
 			prec(PREC_BUILTIN, seq(/\d+\./, $.macro)),
 			prec(PREC_BUILTIN, seq(optional("-"), $.macro, /\.\d+/)),
+		),
+
+	hexadecimal: ($) =>
+		choice(
+			token(prec(PREC_BUILTIN, /0x[0-9a-fA-F]+/)),
+			prec(PREC_BUILTIN, seq(/0x/, $.macro)),
 		),
 
 	greedy_string: (_) => /[^\r\n]+/,
