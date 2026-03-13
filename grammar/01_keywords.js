aa5a1a2a96c5ad9d0843b886df0a5e11163cc3f1
BubbleFish <daforsastudia@gmail.com>
2026-03-13 18:03:39 +0300

    feat(grammar): Add resource

--- grammar/01_keywords.js
+++ grammar/01_keywords.js
@@ -2,7 +2,7 @@ const COMMAND_KEYWORDS = require("../data/command_keywords.js");
 const SUBCOMMAND_KEYWORDS = require("../data/execute_subcommands.js");
 
 module.exports = {
-	command_keyword: (_) => token(prec(1, choice(...COMMAND_KEYWORDS))),
+	command_keyword: (_) => token(choice(...COMMAND_KEYWORDS)),
 	subcommand_keyword: (_) => token(prec(1, choice(...SUBCOMMAND_KEYWORDS))),
 
 	_keywords: ($) =>
--- grammar/03_composite.js
+++ grammar/03_composite.js
@@ -7,6 +7,7 @@ module.exports = {
 			$.vector,
 			$.range,
 			$.path,
+			$._resource,
 			// $.resource,
 			// $.nbt_array,
 			// $.nbt_compound,
@@ -57,96 +58,36 @@ module.exports = {
 			),
 		),
 
-	_data_path_node: ($) => choice(),
-	// $.nbt_compound,
-	// seq($.word, $.nbt_compound),
-	// $.nbt_array,
-	// seq($.word, $.nbt_array, optional($.nbt_array)),
+	// _data_path_node: ($) => choice(),
 
-	// path: ($) =>
-	// 	prec(
-	// 		PREC_COMPOSITE,
-	// 		seq(
-	// 			optional("#"),
-	// 			choice(
-	// 				$.word,
-	// 				$.path_word,
-	// 				seq(
-	// 					$._path_segment,
-	// 					repeat1(seq(token(prec(4, ".")), $._path_segment)),
-	// 				),
-	// 			),
-	// 		),
-	// 	),
-	//
-	// _path_segment: ($) =>
-	// 	prec(
-	// 		PREC_COMPOSITE,
-	// 		choice($.path_word, $.nbt_array, $.nbt_compound, $.integer),
-	// 	),
-	//
-	// path_word: (_) =>
-	// 	token(
-	// 		prec(
-	// 			1,
-	// 			choice(
-	// 				/[a-zA-Z_\-\+\*\?\$%][0-9a-zA-Z_\-\+\*\?%]*/,
-	// 				/[a-zA-Z_\-\+\*\?%]/,
-	// 			),
-	// 		),
-	// 	),
-	//
-	// resource: ($) => choice($.minecraft_resource, $.generic_resource),
-	//
-	// minecraft_resource: ($) => prec(PREC_COMPOSITE, seq("minecraft:", $.word)),
-	//
-	// generic_resource: ($) =>
-	// 	prec.right(
-	// 		PREC_COMPOSITE,
-	// 		seq(
-	// 			choice(
-	// 				seq(optional("#"), $.namespace, ":"),
-	// 				alias(/\.+\//, $.path_word),
-	// 			),
-	// 			$._resource_segment,
-	// 			repeat(seq("/", $._resource_segment)),
-	// 		),
-	// 	),
-	//
-	// _resource_segment: ($) => choice($.word, $.path_word, $.macro),
-	//
-	// namespace: ($) =>
-	// 	seq(
-	// 		$._resource_segment,
-	// 		repeat(seq(token(prec(5, ".")), $._resource_segment)),
-	// 	),
-	//
-	// nbt_array: ($) =>
-	// 	seq(
-	// 		"[",
-	// 		optional(seq($.nbt_array_type, ";")),
-	// 		optional(ARRAY_CONTENTS($._compound_value)),
-	// 		"]",
-	// 	),
-	//
-	// nbt_array_type: (_) => token(prec(PREC_COMPOSITE, /[ISL]/)),
-	//
-	// nbt_compound: ($) =>
-	// 	seq("{", optional(ARRAY_CONTENTS($.key_value_pair)), "}"),
-	//
-	// data_compound: ($) => seq("[", ARRAY_CONTENTS($.key_value_pair), "]"),
-	//
-	// key_value_pair: ($) =>
-	// 	seq(
-	// 		$.compound_key,
-	// 		choice("=", ":", "~"),
-	// 		optional("!"),
-	// 		$._compound_value,
-	// 	),
-	//
-	// compound_key: ($) =>
-	// 	prec(PREC_COMPOSITE, choice($.path_word, $.resource, $.string)),
-	//
-	// _compound_value: ($) =>
-	// 	choice($._primitive_type, $.nbt_array, $.nbt_compound),
+	_resource: ($) => choice($.minecraft_resource, $.generic_resource),
+
+	minecraft_resource: ($) => seq("minecraft:", $.word),
+
+	generic_resource: ($) =>
+		choice(
+			seq(
+				"#",
+				$._resource_segment,
+				repeat1(seq("/", $._resource_segment)),
+			),
+			seq(
+				choice(
+					alias($.word, $.namespace),
+					alias($.score_holder, $.namespace),
+					alias($.path, $.namespace),
+				),
+				":",
+				$._resource_segment,
+				repeat(seq("/", $._resource_segment)),
+			),
+			seq(
+				"./",
+				$._resource_segment,
+				repeat(seq("/", $._resource_segment)),
+			),
+		),
+
+	_resource_segment: ($) =>
+		choice($.macro, alias(/[0-9a-zA-Z\-\+\.\*\?_]+/, $.word)),
 };
