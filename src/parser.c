44ec757bfa2a6bf2e39d3ce2d1cc288941a4ca59
BubbleFish <daforsastudia@gmail.com>
2026-03-25 14:50:34 +0300

    Release 0.3.6

--- src/parser.c
+++ src/parser.c
@@ -102280,7 +102280,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_mcfunction(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 3,
-      .patch_version = 5,
+      .patch_version = 6,
     },
   };
   return &language;
