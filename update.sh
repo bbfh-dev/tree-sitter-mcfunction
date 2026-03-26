a19bc924e923a4cd3bf30cd501a8a0316534de5c
BubbleFish <daforsastudia@gmail.com>
2026-03-26 12:05:31 +0300

    ci: Add fuzzy and queries testing

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 VERSION=$1
 if [ -z $VERSION ]; then
@@ -9,6 +10,7 @@ fi
 tree-sitter version $VERSION
 npx tree-sitter-cli@0.26.7 generate
 cargo test
+ts_query_ls check -f queries/
 git commit -am "Release $VERSION"
 git tag -- v$VERSION
 git push --tags origin main
